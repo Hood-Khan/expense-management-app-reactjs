367451d249da1801138eb09864c7ee1642e4037e
HoodKhan <muhammadhoodkhan9366@gmail.com>
2025-12-31 23:13:02 +0500

    table reposiveness

--- src/components/ListExpense.jsx
+++ src/components/ListExpense.jsx
@@ -3,33 +3,34 @@ import { mainContext } from '../context/MainContext'
 import ExpenseCard from './ExpenseCard'
 
 function ListExpense() {
-  const{allExpense} = useContext(mainContext)
+  const { allExpense } = useContext(mainContext)
   return (
     <>
-      <table className='w-full border table-auto my-10 py-3'>
-        <thead>
-          <tr>
-          <th className='border text-zinc-900 border-gray-500 py-2'>No</th>
-          <th className='border text-zinc-900 border-gray-500'>Price</th>
-          <th className='border text-zinc-900 border-gray-500'>Description</th>
-          <th className='border text-zinc-900 border-gray-500'>Purpose</th>
-          <th className='border text-zinc-900 border-gray-500'>Action</th>
-        </tr>
-        </thead>
-        
-        <tbody>
-          {
-            allExpense && allExpense.length>0 ? <>
-              {
-                  allExpense.map((cur,i)=>{
-                  return <ExpenseCard data={cur} no={i+1} key={i}/>
-                })
-              }
-            </>:<></>
-            
-          }
-        </tbody>
-      </table>
+      <div className="overflow-x-auto my-10">
+        <table className='w-full border table-auto min-w-[600px]'>
+          <thead>
+            <tr>
+              <th className='border text-zinc-900 border-gray-500 py-2'>No</th>
+              <th className='border text-zinc-900 border-gray-500'>Price</th>
+              <th className='border text-zinc-900 border-gray-500'>Description</th>
+              <th className='border text-zinc-900 border-gray-500'>Purpose</th>
+              <th className='border text-zinc-900 border-gray-500'>Action</th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            {allExpense && allExpense.length > 0 ? (
+              allExpense.map((cur, i) => (
+                <ExpenseCard data={cur} no={i + 1} key={i} />
+              ))
+            ) : (
+              <tr>
+                <td colSpan="5" className="text-center py-3">No expenses found</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
